918d387ba430f052651e9d0da7c10965dbb6314a
Jacob Rintjema <jacobrintjema@MacBook-Pro.local>
2020-05-10 16:06:17 -0400

    updated web

--- stockWeb/urls.py
+++ stockWeb/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('active_stocks/', views.activeS, name='active-stocks'),
+    path('idk.html', views.idk, name='idk'),
+    path('historical.csv', views.historical, name='lol'),
     path('stock/<int:stock_id>/', views.stock_detail, name='stock_detail')
 ]
